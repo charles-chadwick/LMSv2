7c0e188e4ee92ce9db9ac75880b9682b58e8d112
Charles <charles@mutuallyassured.com>
2026-07-04 14:28:34 -0700

    Add NewMessage notification (database + broadcast)

---
+++ app/Notifications/NewMessage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
+
+class NewMessage extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(public Message $message) {}
+
+    /**
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'broadcast'];
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'conversation_id' => $this->message->conversation_id,
+            'sender_name' => $this->message->sender->name,
+            'excerpt' => Str::limit($this->message->body, 80),
+            'type' => 'new_message',
+        ];
+    }
+
+    public function toBroadcast(object $notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage($this->toArray($notifiable));
+    }
+}
